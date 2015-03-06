d612f302a73030b277e9c05f2377ec9b960d2638
tushar729 <kingtushar7772gmail.com>
2015-03-06 10:34:22 +0600

    first commit

---
+++ AccountOperationApp/AccountOperationApp/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationApp
+{
+    class Account
+    {
+        public string AccountNumber { get; set; }
+        public string CustomerName { get; set; }
+        public decimal Balance { get; set; }
+
+        public decimal Deposit(decimal amount)
+        {
+           return Balance += amount;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            return Balance -= amount;
+        }
+
+        public string Report()
+        {
+            return CustomerName + ", your account number: " + AccountNumber + " and it's balance: " + Balance + " taka";
+        }
+    }
+}
---
+++ AccountOperationApp/AccountOperationApp/AccountOperationUI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationApp
+{
+    public partial class AccountOperationUI : Form
+    {
+        Account aAccount = new Account();
+        public AccountOperationUI()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            aAccount.AccountNumber = accountNumberTextBox.Text;
+            aAccount.CustomerName = customerNameTextBox.Text;
+            aAccount.Balance = 0.0M;
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            string report = aAccount.Report();
+            MessageBox.Show(report);
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            aAccount.Deposit(decimal.Parse(amountTextBox.Text));
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            aAccount.Withdraw(decimal.Parse(amountTextBox.Text));
+        }
+    }
+}
